33f1c039d75cffeabcb6d43ca61d548928894e79
daniel-cox <ghostseal3@gmail.com>
2023-01-22 13:27:47 -0600

    added new html and css adjustments

---
+++ js/index.js
@@ -0,0 +1,6 @@
+// let hamburger = document.getElementById('navbar-toggler');
+// let menu = document.getElementById('navbar');
+
+// hamburger.addEventListener('click', function() {
+//     menu.classList.toggle('hidden');
+// });
