334f79f5df4741453fea819467c13b9a23a004a3
daniel-cox <ghostseal3@gmail.com>
2023-01-22 14:37:15 -0600

    removed depricated files

--- js/index.js
+++
@@ -1,6 +0,0 @@
-// let hamburger = document.getElementById('navbar-toggler');
-// let menu = document.getElementById('navbar');
-
-// hamburger.addEventListener('click', function() {
-//     menu.classList.toggle('hidden');
-// });
